f424bef38039785e74c9ab7c4d891779f159b866
Ed Bueler <elbueler@alaska.edu>
2024-02-08 09:26:08 -0900

    fiddle with doc

--- py/8feb/block.py
+++ py/8feb/block.py
@@ -1,14 +1,19 @@
-# example showing:
+# This example shows:
 #   1. reading a 2D mesh from a Gmsh file
-#   2. complicated domain (more than a square ...)
+#   2. a complicated domain (more than a square ...)
 #   3. Neumann boundary conditions
 #   4. optional uniform mesh refinement
 #   5. optional higher-order elements
+# The PDE problem solved is documented in problem.pdf,
+# and the domain and mesh setup are documented in
+# geometry.pdf.  Before you can run this code you
+# must use Gmsh to convert the geometry description
+# to a readable .msh file:
+#   gmsh -2 mesh.geo    # generates mesh.msh
 
 from firedrake import *
 
-# read mesh from a .msh file
-#   run to get mesh.msh:  gmsh -2 mesh.geo
+# see above to generate mesh.msh
 mesh = Mesh('mesh.msh')
 
 # optional uniform refinement: in each refinement,
