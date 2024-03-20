9806029c6476cca75279ebe84247a38174ed4d7c
Ed Bueler <elbueler@alaska.edu>
2024-03-19 19:17:03 -0800

    reasonable pair of examples

--- py/21mar/bratu.py
+++ py/21mar/bratu.py
@@ -1,27 +1,41 @@
+# This example solves the nonlinear and steady-state Liouville-Bratu equation
+#   - u_xx - u_yy - lambda e^u = 0
+# on the unit square, with zero Dirichlet boundary conditions.  The equation
+# is a model for thermal runaway, for instance in chemical explosives; see
+# https://en.wikipedia.org/wiki/Liouville%E2%80%93Bratu%E2%80%93Gelfand_equation
+# Note that nonlinear PDEs can have critical parameter values where solutions
+# cease to exist. This code demonstrates basic Newton iteration, with corresponding
+# PETSc SNES options.
+# Documentation:  slides/21mar-bratu.pdf
+
 from firedrake import *
-from firedrake.output import VTKFile  # delete this line in older Firedrake versions
+from firedrake.output import VTKFile  # in older Firedrake versions: delete this line
 
-lam = 6.0  # parameter in Bratu equation; critical value between 6.0 and 7.0?
+lam = 6.0   # parameter in Bratu equation; critical value occurs between 6.0 and 7.0
 
-M = 100
+M = 20      # critical value affected by resolution; M needs to be large enough
+
 mesh = UnitSquareMesh(M,M)
-
 H = FunctionSpace(mesh,'CG',1)
 u = Function(H, name='u_h(x,y)')
 v = TestFunction(H)
 
-x, y = SpatialCoordinate(mesh)
-d2 = (x - 0.8)**2 + (y - 0.1)**2
-f = Function(H).interpolate(exp(-40.0 * d2))   # spot source
-f.rename('f(x,y)')
+# nonlinear weak form because of "exp(u)"
+F = ( dot(grad(u),grad(v)) - lam * exp(u) * v ) * dx
 
-F = ( dot(grad(u),grad(v)) - lam * exp(u) * v - f*v ) * dx
+# optionally add a source term, for example:
+# x, y = SpatialCoordinate(mesh)
+# d2 = (x - 0.8)**2 + (y - 0.1)**2
+# f = Function(H).interpolate(exp(-40.0 * d2))   # spot source
+# f.rename('f(x,y)')
+# F -= f * v * dx
 
 bdry_ids = (1, 2, 3, 4)   # four sides of boundary
 BCs = DirichletBC(H, Constant(0.0), bdry_ids)
 
 solve(F == 0, u, bcs=[BCs],
       solver_parameters = {'snes_type': 'newtonls',
+                           'snes_linesearch_type': 'basic', # or 'bt'?
                            'snes_monitor': None,
                            'snes_converged_reason': None,
                            'ksp_type': 'preonly',
@@ -30,4 +44,5 @@ solve(F == 0, u, bcs=[BCs],
 normu = norm(u, norm_type='L2')
 print(f'{M:d} x {M:d} mesh: |u| = {normu:.6f}')
 
-VTKFile("result.pvd").write(f,u)  # older versions of Firedrake use "File()"
+print('writing solution to result.pvd ...')
+VTKFile("result.pvd").write(u)  # older versions of Firedrake use "File()"
---
+++ py/21mar/plate.py
@@ -0,0 +1,79 @@
+# This example solves a linear system of PDEs, i.e. a vector PDE:
+#   - v_xx - v_yy               = f(x,y)
+#   - v           - u_xx - u_yy = 0
+# for  w = [v(x,y); u(x,y)].  The discretized system produces a block-
+# structured matrix problem
+#   [  L   0 ] = [ f ]
+#   [ -I   L ] = [ 0 ]
+# where L is the usual discretization of the Laplacian.  This system
+# models an elastic plate, and u(x,y) satisfies the scalar fourth-order
+# "pure bending" plate equation:
+#   u_xxxx + 2 u_xxyy + u_yyyy = 0
+# (See: https://en.wikipedia.org/wiki/Plate_theory)
+# This code demonstrates basic "fieldsplit" preconditioning options to
+# solve the block-structured system by an additive multigrid method.
+# Documentation:  slides/21mar-plate.pdf
+
+from firedrake import *
+from firedrake.output import VTKFile  # in older Firedrake versions: delete this line
+
+M = 40    # double M several times to see solver scaling
+          # at M=640 the multigrid solver is distinctly faster
+
+# mesh and 2 component function space
+mesh = UnitSquareMesh(M,M)
+H = FunctionSpace(mesh, 'CG', 1)
+W = H * H            # w = [v; u] is in this space
+w = Function(W, name='w_h(x,y) vector solution')
+v, u = split(w)
+r, s = TestFunctions(W)
+
+# load
+# x, y = SpatialCoordinate(mesh)
+f = Constant(1.0)  # FIXME set up exact solution from Chapter 7 example
+
+# weak form:  F == 0  for all r,s
+F = ( dot(grad(v), grad(r)) - f * r \
+      - v * s + dot(grad(u), grad(s)) ) * dx
+
+bdry_ids = (1, 2, 3, 4)   # four sides of boundary
+BCs = DirichletBC(H, Constant(0.0), bdry_ids)
+BCs = [ DirichletBC(W.sub(0), Constant(0.0), bdry_ids),
+        DirichletBC(W.sub(1), Constant(0.0), bdry_ids) ]
+
+# any solver can use these options
+par = {'snes_type': 'ksponly',
+    'ksp_type': 'gmres',
+    #'ksp_view': None,
+    #'ksp_monitor': None,
+    'ksp_converged_reason': None}
+
+# choose between these two "-pc_type fieldsplit" solvers
+FSmulLU = {'pc_type': 'fieldsplit',
+    'pc_fieldsplit_type': 'multiplicative',
+    'fieldsplit_0_ksp_type': 'preonly',
+    'fieldsplit_0_pc_type': 'lu',
+    'fieldsplit_1_ksp_type': 'preonly',
+    'fieldsplit_1_pc_type': 'lu'}
+FSaddMG = {'pc_type': 'fieldsplit',
+    'pc_fieldsplit_type': 'additive',
+    'fieldsplit_0_ksp_type': 'preonly',
+    'fieldsplit_0_pc_type': 'gamg',
+    'fieldsplit_1_ksp_type': 'preonly',
+    'fieldsplit_1_pc_type': 'gamg'}
+
+par.update(FSmulLU)    # use direct solver
+#par.update(FSaddMG)   # use scalable multigrid-preconditioned iterative solver
+
+solve(F == 0, w, bcs=BCs, solver_parameters = par)
+
+v = w.subfunctions[0]
+v.rename('v_h(x,y) moment')
+u = w.subfunctions[1]
+u.rename('u_h(x,y) displacement')
+normv = norm(v, norm_type='L2')
+normu = norm(u, norm_type='L2')
+print(f'{M:d} x {M:d} mesh: |v| = {normv:.6f}, |u| = {normu:.6f}')
+
+print('writing solution v, u to result.pvd ...')
+VTKFile("result.pvd").write(v, u)  # older versions of Firedrake use "File()"
